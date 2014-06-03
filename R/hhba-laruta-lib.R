48921f033fe95f0ace4228c47e9f093a48686ca4
alejandro baranek <alejandrobaranek@gmail.com>
2014-06-03 10:33:00 -0300

    error en nombres de archivo treemap

--- R/hhba-laruta-lib.R
+++ R/hhba-laruta-lib.R
@@ -117,7 +117,8 @@ make_treemap_por_anio<-function(data){
           tipo='proyectado'
         if (ty=='EJEC')
           tipo='ejecutado'
-        file_name<-paste(home_graficos,'presupuesto_treemap_',anio,'_', tipo)
+        #file_name<-paste(home_graficos,'presupuesto_treemap_',anio,'_', tipo,sep="")
+        file_name<-paste(home_graficos,'presupuesto_',anio,'_', tipo,sep="")
         print(paste('elaborando',file_name))
         png(file_name, height=heightPNG, width=widthPNG, bg="white")   
         treemap(data_anio,c('categoria','juris_ok'),vSize='value',title=paste("Presupuesto Nacional",tipo, " en año",anio))
