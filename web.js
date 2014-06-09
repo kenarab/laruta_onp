2d883c9f4945074b174f5f70e5a6c598804916a1
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2014-06-09 11:34:32 -0300

    Node web.js

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var http = require('http');
 var gzippo = require('gzippo');
+
 var app = express();
 app.use(express.logger());
 app.use(gzippo.staticGzip('' + __dirname));
+
 var server = http.createServer(app);
-server.listen(process.env.PORT || 5000);
